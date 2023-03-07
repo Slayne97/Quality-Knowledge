f83060dcbeaaf84fb325aae51ea48fbcc4ea70cb
Slayne97 <eduardoc_97@hotmail.com>
2023-03-07 11:31:35 -0600

    Added a syllabus

--- test.py
+++ test.py
@@ -1,1 +1,31 @@
-print('asdasd')
+# Gradient descent for linear regression
+# yhat = wx + b
+# loss = (y-yhat)**2 / N
+# Initialize some parameters
+# Create gradient descent function
+# Iteratively make updates
+
+
+import numpy as np
+
+x = np.random.randn(10, 1)
+y = 2*x + np.random.rand()
+w = 0.0
+b = 0.0
+learning_rate = 0.0
+
+
+def descend(x, y, w, b, learning_rate):
+    dldw = 0.0
+    dldb = 0.0
+    N = x.shape[0]
+
+    for xi, yi in zip(x,y):
+        dldw = 
+
+
+for epoch in range(400):
+
+
+
+
